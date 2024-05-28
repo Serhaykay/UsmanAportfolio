dd282b5b701e5acbb60886cf98678ace0fa53f0f
Ser Haykay <92628969+Serhaykay@users.noreply.github.com>
2024-05-28 15:27:02 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 (function () {
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+  [...document.querySelectorAll(".control")].forEach((button) => {
+    button.addEventListener("click", function () {
+      document.querySelector(".active-btn").classList.remove("active-btn");
+      this.classList.add("active-btn");
+      document.querySelector(".active").classList.remove("active");
+      document.getElementById(button.dataset.id).classList.add("active");
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
-        document.body.classList.toggle("light-mode");
-    })
+  });
+  document.querySelector(".theme-btn").addEventListener("click", () => {
+    document.body.classList.toggle("light-mode");
+  });
 })();
+const downloadButton = document.getElementById("downloadButton");
+downloadButton.onclick = function () {
+  downloadPDF("document/MyCv.pdf", "document/MyCv.pdf"); // Replace with your actual PDF URL and desired filename
+};
